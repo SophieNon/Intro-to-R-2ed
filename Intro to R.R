208ae9a6872dc11829053bac2e47442bdc64c2fa
SophieNonikashvili <son223@lehigh.edu>
2020-08-30 13:47:32 +0400

    added DOB

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Copy for SophieNonikashvili 2019
+# Copy for SophieNonikashvili2001 2019
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
